ccce2e8f24811b176dc6c2d4295be9032cdf531d
Leon <h05p1tal13r.leon@gmail.com>
2022-05-20 17:33:03 +0200

    add Car structure, c function example

--- c/main.c
+++ c/main.c
@@ -4,3 +4,31 @@ int test() {
     printf("Bonjour le monde!\n");
     return 0;
 }
+
+void EnterCar()
+{
+    int nbcar;
+    printf("enter the number of car");
+    scanf("%d", nbcar);
+    car tabcar[nbcar];
+    for (int i=0, i<nbcar, i++)
+    {
+        printf("enter the capacity of vehicule", i+1);
+        scanf ("%d", tabcar[i].capacity);
+        printf("enter the battery of vehicule", i+1);
+        scanf ("%d", tabcar[i].battery);
+        printf("enter the recharging time of vehicule", i+1);
+        scanf ("%f", tabcar[i].rechargetps);
+    }
+}
+
+void readfile()
+{
+    fopen()
+}
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
