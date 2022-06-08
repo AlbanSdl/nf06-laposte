4616cd36e21ec82b3ef8aab2bbb476588040b08e
Alban Sdl <35579652+AlbanSdl@users.noreply.github.com>
2022-06-08 19:08:03 +0200

    C things (first part) (#1)
    * add c things
    
    * fix bugs
    
    * remove type hints

--- c/main.c
+++ c/main.c
@@ -1,5 +1,212 @@
 #include "main.h"
+#include <stdlib.h>
 
-int* compute_path(Node nodes[], int nbnodes, Car cars[], int nbcar) {
-    //
+typedef struct {
+    int id;
+    float distance;
+} DistanceRef;
+
+typedef enum {
+    DELIVERED, PENDING, ERRORED
+} Status;
+
+DistanceRef* get_closest_neighbours(Node *node, int nodeCount) {
+    DistanceRef* neighbours = malloc(sizeof(DistanceRef) * nodeCount);
+    
+    for (int i = 0; i < nodeCount; i++) {
+        DistanceRef ref = {
+            id: i,
+            distance: node->distances[i]
+        };
+        neighbours[i] = ref;
+    }
+
+    // Sort distances
+    for (int i = 0; i < nodeCount - 1; i++) {
+        float minDistance = neighbours[i].distance;
+        int minIndex = i;
+        for (int j = i + 1; j < nodeCount; j++) {
+            if (neighbours[j].distance < minDistance) {
+                minDistance = neighbours[j].distance;
+                minIndex = j;
+            }
+        }
+        // Perform permutation
+        DistanceRef temp = neighbours[i];
+        neighbours[i] = neighbours[minIndex];
+        neighbours[minIndex] = temp;
+    }
+
+    return neighbours;
+}
+
+
+void sort_cars_by_battery(Car *cars, int nbcar) {
+    for (int i = 0; i < nbcar - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < nbcar; j++)
+            if (cars[minIndex].battery > cars[j].battery)
+                minIndex = j;
+        Car temp = cars[i];
+        cars[i] = cars[minIndex];
+        cars[minIndex] = temp;
+    }
+}
+
+
+void compute_path(Node nodes[], int nbnodes, Car cars[], int nbcar, RoutedVehicle* result) {
+    sort_cars_by_battery(cars, nbcar);
+
+    Status status[nbnodes];
+    for (int i = 0; i < nbnodes; i++)
+        status[i] = PENDING;
+    int remainingNodes = nbnodes - 1;
+
+    Node warehouse = nodes[0];
+    DistanceRef* warehouse_distances = get_closest_neighbours(&warehouse, nbnodes);
+
+    // RoutedVehicle* vehicles = malloc(sizeof(RoutedVehicle) * nbcar);
+
+    for (int i = 0; i < nbcar; i++) {
+        RoutedVehicle vehicle = {
+            nodes: malloc(sizeof(int)),
+            time: 0,
+            nodeCount: 0
+        };
+        result[i] = vehicle;
+    }
+
+    while (remainingNodes > 0) {
+        
+        // printf("Remaining nodes %d\n", remainingNodes);
+
+        for (int i = nbcar - 1; i >= 0; i--) {
+            int* deliveredNodes;
+            float time;
+            int deliveryCount = 0;
+
+            // We use this vehicle:
+            Car* vehicle = &cars[i];
+            float battery = vehicle->battery;
+
+            // We will go to this node (at first)
+            for (int i = nbnodes - 1; i > 0; i--) {
+                DistanceRef* ref = &warehouse_distances[i];
+                if (status[ref->id] == PENDING) {
+                    // We can go to this node (and deliver the customer)
+                    Node* node = &nodes[ref->id];
+                    if (node->distances[0] <= battery / 2) {
+                        // We can use this node
+                        battery -= node->distances[0];
+                        status[ref->id] = DELIVERED;
+                        remainingNodes--;
+
+                        deliveryCount++;
+                        time += node->distances[0] * vehicle->speedkmh;
+                        deliveredNodes = malloc(sizeof(int) * deliveryCount);
+                        deliveredNodes[0] = ref->id;
+                        break;
+                    } else {
+                        // We can't use this node
+                        status[ref->id] = ERRORED;
+                        remainingNodes--;
+                    }
+                }
+            }
+
+            // printf("Starting at %d\n", deliveredNodes[0]);
+
+            // Then we go to the closest node
+            while (deliveryCount < vehicle->capacity && remainingNodes > 0) {
+                Node currentNode = nodes[deliveredNodes[deliveryCount - 1]];
+                DistanceRef* ref = get_closest_neighbours(&currentNode, nbnodes);
+                short found = 0;
+
+                for (int i = 1; i < nbnodes; i++) {
+                    if (status[ref->id] == PENDING) {
+                        Node* node = &nodes[ref->id];
+                        if (node->distances[0] <= battery - ref->distance) {
+                            battery -= node->distances[0];
+                            status[ref->id] = DELIVERED;
+                            remainingNodes--;
+
+                            deliveryCount++;
+                            time += node->distances[0] * vehicle->speedkmh;
+                            deliveredNodes = realloc(deliveredNodes, sizeof(int) * deliveryCount);
+                            deliveredNodes[deliveryCount - 1] = ref->id;
+                            found = 1;
+                            break;
+                        }
+                    }
+                }
+
+                if (found == 0) break;
+            }
+
+            deliveredNodes = realloc(deliveredNodes, sizeof(int) * (1 + deliveryCount));
+            deliveredNodes[deliveryCount] = 0;
+            deliveryCount++;
+
+            if (deliveryCount > 1) {
+                RoutedVehicle* rvh = &result[i];
+                if (rvh->nodeCount > 0) {
+                    // We have to refuel the vehicle
+                    time += vehicle->battery / vehicle->rechargetps;
+                }
+
+                rvh->nodes = realloc(rvh->nodes, sizeof(int) * (rvh->nodeCount + deliveryCount));
+                for (int i = 0; i < deliveryCount; i++)
+                    rvh->nodes[rvh->nodeCount + i] = deliveredNodes[i];
+                rvh->nodeCount += deliveryCount;
+                rvh->time += time;
+            }
+        }
+
+        // All vehicles went for one travel
+        // We reiterate that (if needed)
+    }
+}
+
+// DEBUG
+int main() {
+    Node nodes[2] = {
+        {
+            id: 0,
+        },
+        {
+            id: 1,
+        }
+    };
+    nodes[0].distances = malloc(sizeof(float) * 2);
+    nodes[0].distances[0] = 0;
+    nodes[0].distances[1] = 2;
+    nodes[1].distances = malloc(sizeof(float) * 2);
+    nodes[1].distances[0] = 2;
+    nodes[1].distances[1] = 0;
+
+    Car cars[2] = {
+        {
+            battery: 100,
+            capacity: 2,
+            speedkmh: 5,
+            rechargetps: 5
+        },
+        {
+            battery: 90,
+            capacity: 3,
+            speedkmh: 5,
+            rechargetps: 5
+        }
+    };
+    RoutedVehicle* vehicles = malloc(sizeof(RoutedVehicle) * 2);
+    compute_path(nodes, 2, cars, 2, vehicles);
+    for (int i = 0; i < 2; i++) {
+        printf("Vehicle %d: %d nodes\n", i, vehicles[i].nodeCount);
+        for (int j = 0; j < vehicles[i].nodeCount; j++) {
+            printf("%d ", vehicles[i].nodes[j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
--- python/main.py
+++ python/main.py
@@ -1,27 +1,28 @@
-import ctypes
-import networkx as nx
-import numpy as np
-import matplotlib.pyplot as plt
+from ctypes import POINTER, Structure, CDLL, byref, c_float, c_int
 
-# lib = ctypes.CDLL('build/libevrp.dll')
-# lib.enter_car()
 
-A = np.matrix([[0,1,1,0],[1,0,0,0],[1,1,0,1],[0,0,0,0]])
+class Car(Structure):
+    _fields_ = ("capacity", c_int), ("battery", c_int), ("speedkmh", c_float), ("rechargetps", c_float)
 
-# Create DiGraph from A
-G = nx.from_numpy_matrix(A, create_using=nx.DiGraph)
+class Node(Structure):
+    _fields_ = ("id", c_int), ("distances", POINTER(c_float))
 
-# Use spring_layout to handle positioning of graph
-layout = nx.spring_layout(G)
+class RoutedVehicle(Structure):
+    _fields_ = ("nodes", POINTER(c_int)), ("nodeCount", c_int), ("time", c_float)
 
-# Draw the graph using the layout - with_labels=True if you want node labels.
-nx.draw(G, layout, with_labels=True)
 
-# Get weights of each edge and assign to labels
-labels = nx.get_edge_attributes(G, "weight")
-
-# Draw edge labels using layout and list of labels
-nx.draw_networkx_edge_labels(G, pos=layout, edge_labels=labels)
-
-# Show plot
-plt.show()
+lib = CDLL('build/libevrp.dll')
+computePath = lib.compute_path
+result = (RoutedVehicle * 1)()
+computePath(
+    (Node * 2)(Node(0, (c_float * 2)(0, 2)), Node(1, (c_float * 2)(2, 0))),
+    c_int(2),
+    (Car * 1)(Car(2, 100, 50, 5)),
+    c_int(1),
+    byref(result)
+)
+for i in result:
+    print("Length (in nodes, excluding first warehouse)", i.nodeCount, "time", i.time)
+    print("Route: 0", end="->")
+    for j in range(i.nodeCount):
+        print(i.nodes[j], end="->")
