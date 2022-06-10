86bbe335b773af5fbab69adfdd1971e2d976efbc
Leon <h05p1tal13r.leon@gmail.com>
2022-06-10 21:16:16 +0200

    docs: add docs to python code

--- python/main.py
+++ python/main.py
@@ -6,6 +6,11 @@ import matplotlib.pyplot as plt
 
 class Car(Structure):
     _fields_ = ("capacity", c_int), ("battery", c_int), ("speedkmh", c_float), ("rechargetps", c_float)
+    """
+    Python equivalent for the vehicule structure (in C)
+    Linked with C through Ctypes. The _fields_ property defines the fields
+    of the structure so that ctypes bindings are working properly (with the proper c types)
+    """
 
 class Node(Structure):
     _fields_ = ("id", c_int), ("distances", POINTER(c_float))
@@ -14,7 +19,7 @@ class RoutedVehicle(Structure):
     _fields_ = ("nodes", POINTER(c_int)), ("nodeCount", c_int), ("deliveries", c_int), ("distance", c_float), ("time", c_float)
 
 
-def computePath(nodes, cars) -> Array[RoutedVehicle]:
+def computePath(nodes, cars):
     lib = CDLL('build/libevrp.dll')
     computePath = lib.compute_path
     result = (RoutedVehicle * len(cars))()
@@ -26,9 +31,11 @@ def computePath(nodes, cars) -> Array[RoutedVehicle]:
         byref(result)
     )
     return result
+    """
+    call Cprogram
+    """
 
-
-def readFromFile(path: str) -> tuple[list[Node], list[Car]]:
+def readFromFile(path):
     wb_obj = openpyxl.load_workbook(path)
     node_sheet = wb_obj["Feuil1"]
 
@@ -39,12 +46,17 @@ def readFromFile(path: str) -> tuple[list[Node], list[Car]]:
             "x": node_sheet.cell(row=j, column=1).value,
             "y": node_sheet.cell(row=j, column=2).value,
         })
+        """
+        read the coordinates from the Excel file
+        add a node with the coordinates to the list
+        """
 
     c_nodes = []
     for node in nodes:
         c_nodes.append(Node(id=node["id"], distances=(c_float * len(nodes))(
             *[((node["x"] - n["x"]) ** 2 + (node["y"] - n["y"]) ** 2) ** .5 for n in nodes]
         )))
+        """calcul des distance"""
     
     car_sheet = wb_obj["Feuil2"]
     cars = []
@@ -55,13 +67,16 @@ def readFromFile(path: str) -> tuple[list[Node], list[Car]]:
             speedkmh=car_sheet.cell(row=i, column=4).value,
             rechargetps=car_sheet.cell(row=i, column=3).value
         ))
+        """
+        read data concerning the vehicles from the Excel file
+        """
 
     wb_obj.close()
     return (nodes, c_nodes, cars)
 
 
-def displayResult(nodes, routedVehicles: Array[RoutedVehicle]):
-    G=nx.Graph()
+def displayResult(nodes, routedVehicles):
+    G=nx.Graph() ##creating the graph
 
     for node in nodes:
         G.add_node(node["id"], pos=(node["x"], node["y"]))
@@ -69,6 +84,9 @@ def displayResult(nodes, routedVehicles: Array[RoutedVehicle]):
     colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown", "black", "grey"]
 
     nb = 0
+    """
+    adding each point to the graph
+    """
     for i in routedVehicles:
         for j in range(0, i.nodeCount - 1):
             if j == 0:
@@ -80,6 +98,9 @@ def displayResult(nodes, routedVehicles: Array[RoutedVehicle]):
     color = [G[u][v]['color'] for u,v in G.edges()]
     nx.draw(G, pos, with_labels=True, edge_color=color)
     plt.show()
+    """
+    display
+    """
 
 
 (nodes, c_nodes, cars) = readFromFile("lieux.xlsx")
